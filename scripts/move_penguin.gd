fe2d89da94058bc6641f1aef270ba2b462ec00ad
orin635 <omcdonogh635@gmail.com>
2023-12-17 23:13:47 +0000

    Added running animations

--- scripts/move_penguin.gd
+++ scripts/move_penguin.gd
@@ -9,17 +9,29 @@ var speed = 8
 @export var acceleration = 6.0#
 @export var rotation_speed = 12.0
 @onready var spring_arm = $SpringArm3D
-@onready var model = $Body
 
 
 #Animation vars
-@onready var lWing = $L_Wing
 @onready var Body = $Body
+@onready var lWing = $Body/L_Wing
+@onready var rWing = $Body/R_Wing
+@onready var lFoot = $Body/L_Foot
+@onready var rFoot = $Body/R_Foot
+var default_x_rotation
+var default_lfoot_origin
+var default_rfoot_origin
+var wing_rotate_direction = 1
+var foot_rotate_direction = 1
+var foot_points = 100
+var foot_step = 0
+var foot_radius = 1
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	gravity = gravity * 1.8
-	
+	default_x_rotation = Body.rotation.x
+	default_lfoot_origin = lFoot.transform.origin
+	default_rfoot_origin = rFoot.transform.origin
 
 
 func get_input(delta):
@@ -41,24 +53,43 @@ func get_input(delta):
 
 func _process(delta):
 	get_input(delta)
-	#animation(velocity)
+	animation(velocity, delta)
 
 
 func _physics_process(delta):
 	velocity.y += -gravity * delta
 	move_and_slide()
 	
-	if velocity.length() > 1.0:
-		model.rotation.y = lerp_angle(model.rotation.y, spring_arm.rotation.y, rotation_speed * delta)
+
+
+func animation(velocity, delta):
 	
+	if velocity.length() > 1.0:
+		Body.rotation.y = lerp_angle(Body.rotation.y, spring_arm.rotation.y, rotation_speed * delta)
+		if(is_on_floor()):
+			#Rotate Character forward relative to speed
+			Body.rotation.x = default_x_rotation - remap(velocity.length(),0, 16,0, 0.45)
+			#Swing the arms
+			#If arms reach max rotation, rotate back
+			if((default_x_rotation - (lWing.rotation.x * wing_rotate_direction))< -0.9):
+				wing_rotate_direction = wing_rotate_direction * -1
+			lWing.rotation.x = lWing.rotation.x + (remap(velocity.length(),0, 16,0, 0.05)*wing_rotate_direction)
+			rWing.rotation.x = rWing.rotation.x + (remap(velocity.length(),0, 16,0, 0.05)*wing_rotate_direction)
+			
+			#Swing the feet
+			if((default_x_rotation - (lFoot.rotation.x * foot_rotate_direction))< -0.9):
+				foot_rotate_direction = foot_rotate_direction * -1
+			lFoot.rotation.x = lFoot.rotation.x + (remap(velocity.length(),0, 16,0, 0.05)*foot_rotate_direction)
+			rFoot.rotation.x = rFoot.rotation.x + (remap(velocity.length(),0, 16,0, 0.05)*foot_rotate_direction*-1)
+	else :
+		Body.rotation.x = default_x_rotation
+		lWing.rotation.x = default_x_rotation
+		rWing.rotation.x = default_x_rotation
+		rFoot.rotation.x = default_x_rotation
+		lFoot.rotation.x = default_x_rotation
 
 
-func animation(velocity):
-	var transform = Body.transform#
-	transform.basis = transform.basis.rotated(Vector3(0, 1, 0), deg_to_rad(25))
 
-	# Set the updated transform to the mesh instance
-	Body.transform = transform
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
