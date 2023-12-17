4a87f663503ac8d1733d74a5a47da90d632d06b5
orin635 <omcdonogh635@gmail.com>
2023-12-17 20:49:04 +0000

    Reformatted files, fixed few bugs

--- scripts/move_penguin.gd
+++ scripts/move_penguin.gd
@@ -1,8 +1,10 @@
 extends CharacterBody3D
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-@export var speed = 8
+var speed = 8
+@export var default_speed = 8 
 @export var jump_speed = 8.0 
+@export var max_speed = 16
 @export var mouse_sensitivity = 0.002 
 @export var acceleration = 6.0#
 @export var rotation_speed = 12.0
@@ -12,43 +14,78 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var cooldown_timer = 0.0
 var cooldown_duration = 0.1
 
+var run_cooldown_timer = 0.0
+var run_cooldown_duration = 5
+
+#Animation vars
+@onready var lWing = $L_Wing
+@onready var Body = $Body
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	gravity = gravity * 1.8
 	
+
+
 func get_input(delta):
 	var vy = velocity.y
 	velocity.y = 0
 	var input = Input.get_vector("move_right", "move_left", "move_back", "move_forward")
 	var dir = Vector3(input.x, 0, input.y).rotated(Vector3.UP, spring_arm.rotation.y)
+	
+	if Input.is_action_pressed("run"):
+		speed = max_speed
+		acceleration = 9
+	else:
+		speed = default_speed
+		acceleration = 6
+	
 	velocity = lerp(velocity, dir * speed, acceleration * delta)
 	velocity.y = vy
-	
+
+
 func _process(delta):
-	cooldown_timer -= delta
+	get_input(delta)
+	animation(velocity)
+
+
+# Calculate collisions
+func handle_collision(collision):
+	var normal = collision.get_normal()
+	var impulse_direction = -normal
+	print(impulse_direction)
+	# Calculate push force based on penguin's speed
+	var penguin_speed = velocity.length()
+	var push_force = penguin_speed * 3
+	var hit_force = impulse_direction * push_force
+	hit_force.y = 3
+	collision.get_collider().apply_central_impulse(hit_force)
+	cooldown_timer = cooldown_duration
+
 
 func _physics_process(delta):
+	cooldown_timer -= delta
+	run_cooldown_timer -= delta
 	velocity.y += -gravity * delta
-	get_input(delta)
 	move_and_slide()
+	
 	if velocity.length() > 1.0:
 		model.rotation.y = lerp_angle(model.rotation.y, spring_arm.rotation.y, rotation_speed * delta)
 	
-	#Calculate collisions
+	
+	# Collect collisions
 	for i in get_slide_collision_count():
 		var c = get_slide_collision(i)
 		if c.get_collider() is RigidBody3D and cooldown_timer <= 0.0:
-			var normal = c.get_normal()
-			var impulse_direction = -normal
-			# Calculate push force based on penguin's speed
-			var penguin_speed = velocity.length()
-			var push_force = penguin_speed * 2
-			var hit_force = impulse_direction * push_force
-			hit_force.y = 0.5
-			print(hit_force)
-			c.get_collider().apply_central_impulse(hit_force)
-			cooldown_timer = cooldown_duration
+			handle_collision(c)
+
+
+func animation(velocity):
+	var transform = Body.transform#
+	transform.basis = transform.basis.rotated(Vector3(0, 1, 0), deg_to_rad(25))
+
+	# Set the updated transform to the mesh instance
+	Body.transform = transform
 
 func _unhandled_input(event):
 	if event is InputEventMouseMotion:
