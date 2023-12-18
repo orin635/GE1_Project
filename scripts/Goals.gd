5a04b0c72dc53f2d6f1c1484a569e3fc91c0241b
Brendan Mc Cullen <C20729145@mytudublin.ie>
2023-12-18 22:16:34 +0000

    fixed out of bounds and added goal score reset

--- scripts/Goals.gd
+++ scripts/Goals.gd
@@ -13,4 +13,6 @@ func _process(delta):
 
 func _on_area_3d_body_entered(body):
 	if body is RigidBody3D:
+		body.global_transform.origin = Vector3(0,5,0)
+		body.linear_velocity = Vector3.ZERO
 		print("SCORE!!!!")
