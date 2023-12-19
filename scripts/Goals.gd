8bb86fa4aac19be5a9d12449aaf6c8a9702419f3
Brendan Mc Cullen <C20729145@mytudublin.ie>
2023-12-19 19:14:13 +0000

    particles for goal

--- scripts/Goals.gd
+++ scripts/Goals.gd
@@ -4,6 +4,7 @@ signal goalScored
 
 var goalSoundPlayer: AudioStreamPlayer
 var goalScoredMesh: MeshInstance3D
+var goalScoredParticles: GPUParticles3D
 var stayTime: float = 3.0    # Time to stay at the same volume in seconds
 var fadeOutTime: float = 3.0  # Time to fade the sound out in seconds
 var timer: float = 0.0
@@ -12,6 +13,7 @@ func _ready():
 	# Assume that goalSoundPlayer is a child node of this node or set it manually
 	goalSoundPlayer = $AudioStreamPlayer
 	goalScoredMesh = $MeshInstance3D2
+	goalScoredParticles = $GPUParticles3D
 
 func _on_area_3d_body_entered(body):
 	if body is RigidBody3D:
@@ -20,6 +22,7 @@ func _on_area_3d_body_entered(body):
 		body.angular_velocity = Vector3.ZERO
 		emit_signal("goalScored")
 		goalScoredMesh.visible = true
+		goalScoredParticles.visible = true
 		goalSoundPlayer.play()
 		timer = 0.0
 
@@ -34,6 +37,7 @@ func _process(delta):
 		# Stop playing after the entire sequence
 		goalSoundPlayer.stop()
 		goalScoredMesh.visible = false
+		goalScoredParticles.visible = false
 
 	timer += delta
 
